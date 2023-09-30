bf2a4118edab262fb3ed14a67384b42a4f237c39
coderSeth13 <coderseth1313@gmail.com>
2023-09-30 21:06:50 +0300

    Complete Js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Declare Variables for the button and the tooltip
+const toolTip = document.querySelector("[data-tooltip-js]"),
+  shareBtn = document.querySelector("[data-share-btn-js]");
+
+// Toogling the tooltip state with an eventlistener
+shareBtn.addEventListener("click", () => {
+  toolTip.classList.toggle("article__tooltip-toggle");
+});
